ef8061a68e4e40a12f43f8960c8126ea2d4268e9
Trung Vu <trung.vu@adurolife.com>
2020-03-20 07:37:51 +0700

    - data script

---
+++ dbscripts/data.sql
@@ -0,0 +1,50 @@
+INSERT into category(name, code, description, thumb_url)
+ values ("Get Healthy", "get-healthy", "BUILD goals start each day as unmet.", "https://images.unsplash.com/photo-1516526995003-435ccce2be97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80");
+
+INSERT into category(name, code, description, thumb_url)
+values ("Apple Health Goals", "health-kit", "Apple Health goals are linked to the iOS Health app and automatically updated when you open DONE", "https://images.unsplash.com/photo-1537970404888-5d304773278a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
+
+INSERT into category(name, code, description, thumb_url)
+values ("Feel Better", "Feel-Better", "QUIT goals start each day as met.", "https://images.unsplash.com/photo-1531643439684-b25b6c766747?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80");
+
+INSERT into category(name, code, description, thumb_url)
+values ("Be More Product", "Be-More-Product", "QUIT goals start each day as met.", "https://images.unsplash.com/photo-1536060316316-2466bda904f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80");
+
+
+INSERT into category(name, code, description, thumb_url)
+values ("Save Money", "Save-Money", "QUIT goals start each day as met.", "https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80");
+
+INSERT into category(name, code, description, thumb_url)
+values ("Grow Professionally", "Grow-Professionally", "QUIT goals start each day as met.", "https://images.unsplash.com/photo-1495908333425-29a1e0918c5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80");
+
+/*insert data for sub-category*/
+INSERT into sub_category(name, code, description, category_id)
+values ("WORKOUT", "WORKOUT", "", 11);
+
+INSERT into sub_category(name, code, description, category_id)
+values ("MEDIATE", "MEDIATE", "", 11);
+
+INSERT into sub_category(name, code, description, category_id)
+values ("DRINK WATER", "DRINK-WATER", "", 11);
+
+INSERT into sub_category(name, code, description, category_id)
+values ("EAT VEGGIES", "EAT VEGGIES", "", 11);
+
+INSERT into sub_category(name, code, description, category_id)
+values ("EAT BREAKFAST", "EAT BREAKFAST", "", 11);
+
+
+INSERT into sub_category(name, code, description, category_id)
+values ("WALKING STEPS", "WALKING-STEPS", "", 12);
+
+INSERT into sub_category(name, code, description, category_id)
+values ("ACTIVE CATEGORIES BURNED", "ACTIVE-CATEGORIES-BURNED", "", 12);
+
+INSERT into sub_category(name, code, description, category_id)
+values ("EXERCIES MINUTES", "EXERCIES-MINUTES", "", 12);
+
+
+INSERT into sub_category(name, code, description, category_id)
+values ("EXERCIES MINUTES", "EXERCIES-MINUTES", "", 13);
+
+
